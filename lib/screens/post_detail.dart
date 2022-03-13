138c0a9137724f8ae94bb5b89859d54b5a7975ca
dueonkim89 <dueonkim89@yahoo.com>
2022-03-13 14:28:22 -0700

    added post detail page

--- lib/screens/post_detail.dart
+++ lib/screens/post_detail.dart
@@ -23,11 +23,57 @@ class PostDetail extends StatefulWidget {
 }
 
 class _PostDetailState extends State<PostDetail> {
+  Widget showPostDetail() {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        getPostDate(),
+        showImage(),
+        showQuantity(),
+        showUserCoordinates()
+      ],
+    ));
+  }
+
+  // get date post was created
+  Widget getPostDate() {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(35, 30, 35, 40),
+        child: Text(widget.date,
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w600)));
+  }
+
+  // get image of wasted food
+  Widget showImage() {
+    return Image.network(
+      widget.url,
+      height: 275,
+    );
+  }
+
+  // show quantity of wasted food
+  Widget showQuantity() {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(35, 50, 35, 40),
+        child: Text("${widget.quantity.toString()} items",
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w600)));
+  }
+
+  // show user location
+  Widget showUserCoordinates() {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(35, 30, 35, 40),
+        child: Text(
+            "Location: (${widget.lat.toString()},  ${widget.long.toString()})",
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w600)));
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(
-        "${widget.lat}, ${widget.long}, ${widget.quantity}, ${widget.date}, ${widget.url}");
     return ProjectScaffold(
-        title: 'Wasteagram', childComponent: Text("HI!"), showCamera: false);
+        title: 'Wasteagram',
+        childComponent: showPostDetail(),
+        showCamera: false);
   }
 }
